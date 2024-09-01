c8dddfc76e127a8991c09719ce3d8d1ab6b1c530
YevhenKost <kosteugeneo@gmail.com>
2024-09-01 12:55:29 +0100

    added more letters to the alphabet

--- loaders.py
+++ loaders.py
@@ -83,7 +83,7 @@ def load_metric() -> CodeSwitchingNERMetric:
             labelname="MathPower"
         ),
         ArabicWojoodNER(
-            modelpath="ArabicNER-Wojood/"
+            modelpath="ArabicNER-Wojood/ArabicNER-Wojood/"
         ),
         RegexFinder(
             pattern="^#[\w]+",
@@ -148,6 +148,20 @@ def load_metric() -> CodeSwitchingNERMetric:
         'ء', 'أ', 'إ', 'آ', 'ؤ', 'ئ'
     ]
     arabic_alphabet += "".join(hamza_and_variations)
+
+    AR_LETTERS_CHARSET = frozenset(u'\u0621\u0622\u0623\u0624\u0625\u0626\u0627'
+                                   u'\u0628\u0629\u062a\u062b\u062c\u062d\u062e'
+                                   u'\u062f\u0630\u0631\u0632\u0633\u0634\u0635'
+                                   u'\u0636\u0637\u0638\u0639\u063a\u0640\u0641'
+                                   u'\u0642\u0643\u0644\u0645\u0646\u0647\u0648'
+                                   u'\u0649\u064a\u0671\u067e\u0686\u06a4\u06af')
+
+    arabic_alphabet += "".join(list(AR_LETTERS_CHARSET))
+    arabic_alphabet += "".join(['ب','ذ','ق','ج','إ','ز','ة', 'ك', 'ه'
+                                   ,'ص','د','ت','ح','ل','ض','ع','ء','ث','ش','و','ظ','ن','س','غ','ف','ي',
+                                'آ','ر','أ','ؤ','خ','ئ','ط','ى', 'م','ا'])
+
+    arabic_alphabet = "".join(list(set(arabic_alphabet)))
 
 
     metric = CodeSwitchingNERMetric(

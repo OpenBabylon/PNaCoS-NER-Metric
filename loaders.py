eb3e29ae58ff65bd0a62107f9335bab3180e0356
YevhenKost <kosteugeneo@gmail.com>
2024-10-03 13:20:55 +0100

    fix incorrect Ii letters in the alphabet

--- loaders.py
+++ loaders.py
@@ -117,7 +117,7 @@ def load_metric() -> CodeSwitchingNERMetric:
     )
 
     metric = CodeSwitchingNERMetric(
-        origin_alphabet="АаБбВвГгҐґДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщьЮюЯя",
+        origin_alphabet="АаБбВвГгҐґДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпСсТтРрУуХхФфЦцЧчШшЩщьЮюЯя",
         ner_modules=ner_modules,
         sentence_ner=sentence_ner
     )
